c35fb661422e31c0e11826645799f1ea3050c78b
EdgarJC2803 <edgar_kdlc@hotmail.com>
2025-08-22 13:21:27 -0600

    pruebas de funcion

--- app.js
+++ app.js
@@ -2,10 +2,10 @@
 alert("Hola, Bienvenido al Desafio del Amigo Secreto de Alura-Latam!");
 
 
-let numeroSecreto = 0;
+let nombreSecreto = 0;
 let intentos = 0;
-let listaNumerosSorteados = [];
-let numeroMaximo = 10;
+let listaNombresSorteados = [];
+let nombresMaximo = 10;
 
 
 function asignarTextoElemento(elemento, texto) {
@@ -15,17 +15,17 @@ function asignarTextoElemento(elemento, texto) {
 }
 
 function verificarIntento() {
-    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    let nombreDeAmigo = parseInt(document.getElementById('nombreDeAmigo').value);
     
-    if (numeroDeUsuario === numeroSecreto) {
-        asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+    if (nombreDeAmigo === nombreSecreto) {
+        asignarTextoElemento('p',`Acertaste el Nombre en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
         document.getElementById('reiniciar').removeAttribute('disabled');
     } else {
         //El usuario no acertó.
-        if (numeroDeUsuario > numeroSecreto) {
-            asignarTextoElemento('p','El número secreto es menor');
+        if (nombreDeAmigo = nombreSecreto) {
+            asignarTextoElemento('p','El Nombre no coincide');
         } else {
-            asignarTextoElemento('p','El número secreto es mayor');
+            asignarTextoElemento('p','El Nmbre no exixte');
         }
         intentos++;
         limpiarCaja();
@@ -34,51 +34,45 @@ function verificarIntento() {
 }
 
 function limpiarCaja() {
-    document.querySelector('#valorUsuario').value = '';
+    document.querySelector('#nombreDeAmigo').value = '';
 }
 
-function generarNumeroSecreto() {
-    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
-    //si el número generado está en la lista
-    if (listaNumerosSorteados(numeroGenerado)) {
-        return generarNumeroSecreto();
+function generarNombreSecreto() {
+    
+    if (listaNombresSorteados(nombreGenerado)) {
+        return generarNombreSecreto();
     } else {
-        listaNumerosSorteados.push(numeroGenerado);
-        return numeroGenerado;
+        listaNombresSorteados.push(nombreGenerado);
+        return nombreGenerado;
     }
 
-    console.log(numeroGenerado);
-    console.log(listaNumerosSorteados);
-    //Si ya sorteamos todos los números
-    if (listaNumerosSorteados.length == numeroMaximo) {
-        asignarTextoElemento('p','Ya se sortearon todos los números posibles');
+    //Si ya sorteamos todos los nombres
+    if (listaNombresSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p','Ya se sortearon todos los nombres posibles');
     } else {
-        //Si el numero generado está incluido en la lista 
-        if (listaNumerosSorteados.includes(numeroGenerado)) {
-            return generarNumeroSecreto();
+        //Si el nombre generado está incluido en la lista 
+        if (listaNombresSorteados.includes(nombreGenerado)) {
+            return generarNombreSecreto();
         } else {
-            listaNumerosSorteados.push(numeroGenerado);
-            return numeroGenerado;
+            listaNombresSorteados.push(nombreGenerado);
+            return nombreGenerado;
         }
     }
 }
 
 function condicionesIniciales() {
-    asignarTextoElemento('h1','Juego del número secreto!');
-    asignarTextoElemento('p',`Indica un número del 1 al ${numeroMaximo}`);
-    numeroSecreto = generarNumeroSecreto();
+    asignarTextoElemento('p','Juego del Amigo secreto de Alura-Latam!');
+    nombreSecreto = generarNombreSecreto();
     intentos = 1;
-    console.log(numeroSecreto);
+
 }
 
 function reiniciarJuego() {
     //limpiar caja
-    limpiarCaja();
-    //Indicar mensaje de intervalo de números 
-    //Generar el número aleatorio
-    //Inicializar el número intentos
+    limpiarCaja(); 
+
     condicionesIniciales();
-    //Deshabilitar el botón de nuevo juego
+
     document.querySelector('#reiniciar').setAttribute('disabled','true');
     
 }
